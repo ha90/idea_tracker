3f42751b33f5fa0db6be7c913b8179170ec0da87
H Arjun <harjun2090@gmail.com>
2017-04-28 10:33:37 +0530

    Added more detail to Idea model

--- ideaTracker/models.py
+++ ideaTracker/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Idea(models.Model):
-    priority = models.IntegerField()
+    priority = models.IntegerField(default=9)
     title = models.CharField(max_length=140)
     description = models.CharField(max_length=500)
+    status = models.IntegerField(default=0)
+    state = models.IntegerField(default=0)
+    create_date = models.DateTimeField('date_created')
+    modified_date = models.DateTimeField('date_modified')
 
+    def __str__(self):
+        return self.title
--- idea_tracker/settings.py
+++ idea_tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ideaTracker.apps.IdeatrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
