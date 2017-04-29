1ed7b805c40b5582f0b1c99fab91dc3b070dd40d
H Arjun <harjun2090@gmail.com>
2017-04-29 20:20:42 +0530

    Worked more on views.

--- ideaTracker/urls.py
+++ ideaTracker/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<idea_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- ideaTracker/views.py
+++ ideaTracker/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import Idea
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World. Welcome to Idea Tracker.");
+    idea_list = Idea.objects.all()
+    template = loader.get_template('ideaTracker/index.html')
+    context = {
+        'idea_list': idea_list,
+    }
+    return HttpResponse(template.render(context, request));
+
+def detail(request, idea_id):
+    try:
+        idea = Idea.objects.get(pk=idea_id)
+    except Idea.DoesNotExist:
+        raise Http404("Idea does not exist");
+    return render(request, 'ideaTracker/detail.html', {'idea': idea})
+
